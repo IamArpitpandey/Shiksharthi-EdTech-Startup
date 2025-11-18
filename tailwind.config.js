78ca0075fcf4f0104fe737ae05edd6784db7b74d
Ananya Verma <2k23.csiotananyaverma535@gmail.com>
2025-11-18 21:43:01 +0530

    saving local changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,22 +6,18 @@ module.exports = {
   ],
   theme: {
     extend: {
-      theme: {
-  extend: {
-    animation: {
-      blob: "blob 8s infinite ease-in-out",
-      pulseSlow: "pulse 3s infinite ease-in-out",
-    },
-    keyframes: {
-      blob: {
-        "0%, 100%": { transform: "translate(0px, 0px) scale(1)" },
-        "33%": { transform: "translate(30px, -50px) scale(1.1)" },
-        "66%": { transform: "translate(-20px, 20px) scale(0.9)" },
+      animation: {
+        blob: "blob 8s infinite ease-in-out",
+        pulseSlow: "pulse 3s infinite ease-in-out",
+      },
+      keyframes: {
+        blob: {
+          "0%, 100%": { transform: "translate(0px, 0px) scale(1)" },
+          "33%": { transform: "translate(30px, -50px) scale(1.1)" },
+          "66%": { transform: "translate(-20px, 20px) scale(0.9)" },
+        },
       },
     },
   },
-},
-    },
-  },
-  plugins: [require("@tailwindcss/line-clamp")],
+  plugins: [],
 }
